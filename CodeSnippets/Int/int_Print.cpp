5686103cedc0f7c0ac7f4387f33182269453cddb
Omkar Ubale <oubale@gmail.com>
2021-04-09 13:33:17 +0530

    added new snippets: int_Print and vector_PrintElements

---
+++ CodeSnippets/Int/int_Print.cpp
@@ -0,0 +1,6 @@
+void printInt(string name, int value, bool insertEndLine = false)
+{
+    cout<<name<<": "<<value<<"\n";
+    if(insertEndLine)
+        cout<<"\n";
+}
---
+++ CodeSnippets/Vector/vector_PrintElements.cpp
@@ -0,0 +1,8 @@
+void printVector(vector<int> list)
+{    
+    for(int i = 0; i < list.size(); i++)
+    {
+        cout<<list[i]<<" ";
+    }
+    cout<<"\n";
+}
